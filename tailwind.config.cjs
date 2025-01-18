9782b5e134478c0cae13ddce987f8f22b8fc5a37
lonchop <goranjose16@gmail.com>
2025-01-18 19:34:10 -0400

    Add assets Update app name Update index.html Update style.css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['**/*.{html, jsx, js}', '**/*.js', '**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#DCEEF2',
+        secondary: '#BDB8FF',
+      },
+    },
   },
   plugins: [],
 }
